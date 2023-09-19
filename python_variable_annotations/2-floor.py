340ffe8908376a26325d9b3cf7641fa93f75d077
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-18 21:49:46 -0400

    Add exercise 2 of Python type hints

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Has function 'floor'.
+
+Uses Python type hints to specify that
+'floor' takes in a float, and returns a float.
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Assuming that 'n' is a float,
+    returns the floored version of 'n'.
+    """
+    return float(int(float))
