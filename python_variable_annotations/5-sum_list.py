cb7a2c0628fa0ed8ab888b1fbb4d0e7edb63a3ad
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-19 12:35:16 -0400

    Add Python type hints exercise 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Has one function called 'sum_list'
+that takes in an 'input_list',
+which should be a list of floats,
+and returns the 'float' sum of
+the floats inside the list.
+
+Uses a complex type hint to specify that
+sum_list's 'input_list' argument
+should be a list of floats,
+and 'sum_list' returns a 'float'.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Returns the sum of all of the items
+    inside 'input_list', using sum(input_list).
+
+    Assumes that 'input_list' is a list
+    of floats.
+    """
+    return sum(input_list)
