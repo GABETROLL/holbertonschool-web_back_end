79716c3c678d916987321be27f6e0b4f4c765aad
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-18 21:51:57 -0400

    change return type

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,13 +3,15 @@
 Has function 'floor'.
 
 Uses Python type hints to specify that
-'floor' takes in a float, and returns a float.
+'floor' takes in a float, and returns an int.
 """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     Assuming that 'n' is a float,
-    returns the floored version of 'n'.
+    returns the floored version of 'n',
+    which should be an 'int', since 'n'
+    is assumed to be a 'float'.
     """
-    return float(int(float))
+    return int(n)
