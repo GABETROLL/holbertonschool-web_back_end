f2df60845dbf20ea0f2b8bc3b56feec67f9be805
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-19 11:49:58 -0400

    Add python exercise 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Has one function named 'to_str'.
+Uses Python type hints to specify that
+'to_str' takes in a 'float' and returns a 'str'.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Assuming that 'n' is a float,
+    returns the string representation
+    of 'n'.
+    """
+    return str(n)
