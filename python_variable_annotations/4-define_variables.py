10023d1308684817318ae65779c4366bca07c63c
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-19 12:22:11 -0400

    Add annotations exercise 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This file uses type annotations for variables.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+# ^ (this was part of the assignment)
+school: str = "Holberton"
