a803bab4ceba69cc2187939bbf56b2fe80d24a5c
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-19 13:43:52 -0400

    Add Python annotations exercise 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This time, this file uses
+'Tuple' to specify that 'to_kv'
+returns a tuple, and 'Union'
+for the 'v' parameter.
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple with:
+    'k' as the first item,
+    and 'v' as the float representation
+    of v squared.
+
+    Assumes that 'k' is of type 'str'
+    and 'v' is of type 'int' or of type 'float'.
+    """
+    return k, float(v ** 2)
