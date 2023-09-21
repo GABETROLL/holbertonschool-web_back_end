63db43efd6ae87c1b57f939aac1d8900d2975153
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-21 12:10:03 -0400

    Add Python async

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import asyncio
+from random import uniform
+
+async wait_random(max_delay: float = 10):
+    """
+    Waits for a random amount of time
+    (somewhat) in the range of
+    0 and 'max_delay' seconds.
+    """
+    await asyncio.sleep(
+            uniform(0, max_delay)
+    )
