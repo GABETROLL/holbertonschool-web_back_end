f2be03076c18314c4a0b46008781d63e9aa49a5b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-26 18:44:49 -0400

    Add python async comprehensions directory

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+"""
+Make a coroutine called 'async_generator'.
+It should do this 10 timeS:
+    sleep for 1 second,
+    yield a number between 0 and 10.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    An async generator that yields 10 floats.
+
+    Does this 10 times:
+        Sleeps for 1 second,
+        then yields a random float between 0 and 10.
+    """
+    for _ in range(10):
+        asyncio.sleep(1)
+        yield random.random() * 10
---
+++ python_async_comprehension/README.md
@@ -0,0 +1,2 @@
+# Python Async Generators
+
