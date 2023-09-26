8b11fa6ee181152d352d622142c48be3a56ce159
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-26 14:54:02 -0400

    Upgrade cods for exercise 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,22 +3,29 @@
 Imports 'wait_n' 1-concurrent_coroutines.py,
 'asyncio' and 'time' from 'time'.
 
-Uses 'time' make a coroutine that calls 'wait_n',
-measures the amount of time it takes to run,
-and returns it.
+Makes a coroutine that calls 'wait_n(n, max_delay)',
+and using 'time',
+measures the amount of time it took to run,
+then returns it divided by n.
 """
-wait_n = __import__('1-concurrent_coroutines.py').wait_n
 import asyncio
 from time import time
+wait_n = __import__('1-concurrent_coroutines.py').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
     """
-    Runs 'wait_n(n, max_delay)',
-    then returns the amount of time it took to run.
+    Measures the amount of time
+    running 'wait_n(n, max_delay)'.
+
+    Then, this function
+    returns the amount of time that took to run,
+    divided by 'n'.
     """
     before = time.now()
     await wait_n(n, max_delay)
     after = time.now()
 
-    return after - before
+    total_time = after - before
+
+    return total_time / n
