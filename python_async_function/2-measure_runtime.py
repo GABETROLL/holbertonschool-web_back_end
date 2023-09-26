c9dc6de5ce7880aad63cd82a4ddf6c65e84896bd
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-26 15:18:05 -0400

    Turn coroutine into function, and run coroutine with asyncio.run

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,9 +3,11 @@
 Imports 'wait_n' 1-concurrent_coroutines.py,
 'asyncio' and 'time' from 'time'.
 
-Makes a coroutine that calls 'wait_n(n, max_delay)',
+Makes a FUNCTION that calls
+runs 'wait_n(n, max_delay)',
 and using 'time',
-measures the amount of time it took to run,
+measures the amount of time
+'wait_n(...)' took to run,
 then returns it divided by n.
 
 The time is returned as a float,
@@ -17,21 +19,23 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 milliseconds = float
 
-async def measure_time(n: int, max_delay: int) -> milliseconds:
+def measure_time(n: int, max_delay: int) -> milliseconds:
     """
-    Measures the amount of time
-    running 'wait_n(n, max_delay)'.
+    Runs 'wait_n(n, max_delay)' using asyncio.
 
     Then, this function
     returns the amount of time that took to run,
     divided by 'n'.
 
+    The amount of time is measured by using
+    'time.process_time'.
+
     The type of this function's returned result
     is a float representing milliseconds.
     """
     before: milliseconds = time.process_time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     after: milliseconds = time.process_time()
 
-    total_time = after - before
+    total_time: milliseconds = after - before
     return total_time / n
