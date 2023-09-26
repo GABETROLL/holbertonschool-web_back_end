545aa4ad419b324f6c5f1ab251def4de386dfaa3
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-26 16:05:30 -0400

    fix import issue

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,7 +4,7 @@ Make a FUNCTION that takes in an 'int' 'max_delay'
 and returns an 'asyncio.Task'.
 """
 import asyncio
-wait_random = __import__("0-basic_async_syntax")
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
