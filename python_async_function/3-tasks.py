f1c953b3f7ead74ab27ddae5a3f9db8e0e7e64b8
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-26 15:47:08 -0400

    Add Python async exercise 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 """
+Make a FUNCTION that takes in an 'int' 'max_delay'
+and returns an 'asyncio.Task'.
 """
+import asyncio
+wait_random = __import__("0-basic_async_syntax")
 
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Returns 'wait_random(max_delay)' as an 'asyncio.Task'.
+    """
+    return asyncio.create_task(wait_random(max_delay))
