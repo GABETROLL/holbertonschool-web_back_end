e50de5cc927b0e9389a801a1d5b9b2875609fb7e
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-27 11:38:10 -0400

    fix import error for 1 and add its permission

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -12,7 +12,7 @@ USING AN ASYNC COMPREHENSION.
 """
 import asyncio
 from typing import List
-async_generator = __import__("0-async_generator.py").async_generator
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
