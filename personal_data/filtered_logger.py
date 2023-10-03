a4a5a31035a56befe0157ed0082e2345e9ff90be
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-03 19:05:58 -0400

    Obfuscate log message's passwords and dates of birth

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    """
+    'message' should be similar to a CSV format:
+    <field name>=<field value>{separator}<field name>=<field value>{separator}
+    (I believe it should end with a separator)
+
+    Returns 'message', but each field that has a name that's in 'fields'
+    has its value replaces with 'redacion'.
+    """
+    return separator.join(
+        (
+            re.sub(field.split("=")[1], redaction, field)
+                if field.split("=")[0] in fields
+                else field
+            for field in message.split(separator)
+        )
+    )
