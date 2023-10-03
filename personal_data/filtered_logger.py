6a34a0d1b990685b4a69f4d76dccae283a87ac9b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-03 19:13:10 -0400

    Fix pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,11 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
     'message' should be similar to a CSV format:
     <field name>=<field value>{separator}<field name>=<field value>{separator}
@@ -13,10 +17,8 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     has its value replaces with 'redacion'.
     """
     return separator.join(
-        (
-            re.sub(field.split("=")[1], redaction, field)
-                if field.split("=")[0] in fields
-                else field
-            for field in message.split(separator)
-        )
+        re.sub(field.split("=")[1], redaction, field)
+        if field.split("=")[0] in fields
+        else field
+        for field in message.split(separator)
     )
