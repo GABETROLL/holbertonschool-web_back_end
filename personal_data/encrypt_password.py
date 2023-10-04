605ef341f1dd6bd78b66212814774c602ea588ef
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-04 15:17:49 -0400

    Document encrypt_password.py

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+Exercise 5: Make a function that takes in a password string,
+and returns the password salted and hashed using 'bcrypt.hashpw'.
+
+The return type should be a byte string.
+"""
 import bcrypt
 
 
