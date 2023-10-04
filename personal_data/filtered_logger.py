53021c204c0246a5c7b676f05d58768dcec476e6
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-04 14:34:16 -0400

    Add RedactingFormatter class and more tests

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -25,3 +25,21 @@ def filter_datum(
         PATTERN = f"(?<={field_name}=)(.*?)(?={separator}$)"
         result = re.sub(PATTERN, redaction, result)
     return result
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+        self.fields: List[str] = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
--- personal_data/test.py
+++ personal_data/test.py
@@ -4,22 +4,36 @@ Main file
 """
 import unittest
 import random
+from filtered_logger import filter_datum
 
-filter_datum = __import__('filtered_logger').filter_datum
-
-fields = ["password", "date_of_birth"]
+FIELDS = ["password", "date_of_birth"]
+REDACTION = "xxx"
 
 
 class Main(unittest.TestCase):
     def test_filter_datum(self):
 
-        for c1 in range(33, 126 + 1):
-            for c2 in range(33, 126 + 1):
+        for separator_ord in range(33, 126 + 1):
+            separator = chr(separator_ord)
 
-                password = chr(c1) + chr(c2)
+            for c1 in range(33, 126 + 1):
+                for c2 in range(33, 126 + 1):
 
-                self.assertEqual(
-                    filter_datum(fields, "xxx", f"password={password};", ';'),
-                    "password=xxx;",
-                    msg=f"FAILURE: {password}"
-                )
+                    password = chr(c1) + chr(c2)
+
+                    self.assertEqual(
+                        filter_datum(FIELDS, REDACTION, f"password={password}{separator}", separator),
+                        f"password={REDACTION}{separator}",
+                        msg=f"FAILURE: {separator=} {password=}"
+                    )
+            
+            for a in range(31 + 1):
+                for b in range(31 + 1):
+                    for year in range(2023):
+                        date_of_birth = f"{a}/{b}/{year}"
+
+                        self.assertEqual(
+                            filter_datum(FIELDS, REDACTION, f"date_of_birth={date_of_birth}{separator}", separator),
+                            f"date_of_birth={REDACTION}{separator}",
+                            msg=f"FAILURE: {separator=} {date_of_birth=}"
+                        )
