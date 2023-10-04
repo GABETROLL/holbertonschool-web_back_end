259c1612a6ce1a4c30877fa2f069c588f032281f
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-04 15:15:24 -0400

    Encode string before using it

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,6 +5,6 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """
     Returns the password salted and hashed,
-    with 'bcrypt.hashpw(password, bcrypy.gensalt())'.
+    with 'bcrypt.hashpw(password.encode(), bcrypy.gensalt())'.
     """
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
