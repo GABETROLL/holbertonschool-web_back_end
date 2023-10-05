f2860f72ecf39b6bda1a614c60a523e49d26e78a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-04 20:13:14 -0400

    Add docstrings

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+Template to handle all authentication of this project/folder,
+'Basic_authentication/'.
+"""
 from flask import request
 from typing import List, TypeVar
 
@@ -8,10 +13,13 @@ class Auth:
     implemented in this project, called 'Basic_authentication'.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """Authorization header"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
         return None
