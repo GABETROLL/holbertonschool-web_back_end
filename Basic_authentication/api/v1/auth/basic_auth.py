46baa9eb3a7a04797926419872b37663b552f664
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-05 15:28:01 -0400

    Decode the credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ child class of 'Auth'.
 """
 from api.v1.auth.auth import Auth
 import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -39,3 +40,28 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len(HEADER_START):]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        'base64_authorization_header' IS ASSUMED TO BE RETURNED
+        FROM 'self.extract_base64_authorization_header', AND IS ASSUMED
+        TO BE VALID USER CERENDTIALS!!!
+
+        If 'base64_authorization_header' is:
+            None,
+            not a 'str' (even if it's an instance of a child of 'str'),
+            or not a valid Base64 encoding:
+        THIS METHOD RETURNS NONE.
+
+        Decodes and returns the <username: password> credentials that are
+        encoded in 'base64_authorization_header', in Base64.
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+
+        try:
+            return base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
