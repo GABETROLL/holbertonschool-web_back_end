f2779a9a1df94110b0b7af35f335913cb47a38dd
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-04 20:27:50 -0400

    Implement require_path

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -13,8 +13,34 @@ class Auth:
     implemented in this project, called 'Basic_authentication'.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require auth"""
-        return False
+        """
+        'path': the path the user is trying to request.
+
+        'excluded_paths': the paths the user needs authentication
+        when requesting them.
+
+        THIS METHOD ASSUMES THAT ALL OF THE PATHS IN 'excluded_paths'
+        END WITH A '/'.
+
+        Returns True if 'path' is not inside 'excluded_paths'.
+        IF 'path' DOESN'T END WITH A '/', 'path' FIRST GETS
+        THE ENDING '/' BEFORE CHECKING.
+
+        If 'path' is in 'excluded_paths', the user needs
+        authentication when requesting it.
+        If it's not in 'excluded_paths',
+        the user doesn't need authentication when requesting it.
+
+        This method returns True if 'path' is None
+        or 'excluded_paths' is None or empty.
+        """
+        if path is None or excluded_paths is None:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
