ce92db0b62daaefbecb18ae665c36ecfa207faff
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-16 16:13:08 -0400

    Add Unittests_and_integration_tests/

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,40 @@
+import unittest
+import parameterized
+import utils
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Tests that 'utils.access_nested_map' works correctly.
+    """
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b", 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ]
+    )
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any) -> None:
+        """
+        Tests that 'utils.access_nested_map' returns
+        what it's supposed to.
+
+        It is supposed to return the value at the end
+        of the path through each nested mapping,
+        like a file path:
+
+        >>> access_nested_map({"a": 1}, ("a",))
+        1
+        >>> access_nested_map({"a": {"b": 2}}, ("a",))
+        {"b": 2}
+        >>> access_nested_map({"a": {"b": 2}}, ("a", "b"))
+        2
+        """
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
