caa4f8bd0014ae5807a6b0800f02762ebac72146
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-31 11:38:35 -0400

    Return the key, and add main test

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -17,5 +17,18 @@ class Cache:
         Stores <data> as a value with
         a uuid key in the Redis database
         stored in <self>.
+
+        This method then returns the uuid key,
+        as a string.
         """
-        self._redis.set(str(uuid4()), data)
+        KEY = str(uuid4())
+        self._redis.set(KEY, data)
+        return KEY
+
+
+if __name__ == "__main__":
+    cache = Cache()
+    KEY = cache.store("Hello, World!")
+    print(f"KEY: {KEY}")
+    VALUE = cache._redis.get(KEY)
+    print(f"VALUE: {VALUE}")
