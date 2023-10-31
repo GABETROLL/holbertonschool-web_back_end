73ed91cfe50b5a6baa0a8fd308f08d8f5bb80ff8
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-31 15:49:55 -0400

    Add web.py

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,27 @@
+"""
+Has <DB = redis.Redis()>, that keeps track
+of the amount of calls per URL through <get_page>
+in the last 10 seconds,
+and <get_page>.
+"""
+import requests
+import redis
+
+DB = redis.Redis()
+
+
+def get_page(url: str) -> str:
+    """
+    Makes a request to <url>.
+    Increments the cache counter for the <url>
+    in DB, under the <f"count:{url}"> key.
+    Sets the key's expiration time of 10 seconds.
+    Returns the result.
+    """
+    URL_HTML = requests.get(url).content
+
+    URL_CACHE_KEY = f"count:{url}"
+    DB.incr(URL_CACHE_KEY)
+    DB.expire(URL_CACHE_KEY, 10)
+
+    return URL_HTML
