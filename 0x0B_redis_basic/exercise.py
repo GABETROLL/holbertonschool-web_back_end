0ebaabdc559173257a3e6710185d0ccc7d49909f
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-31 12:33:27 -0400

    Add count_calls decorator

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,8 +3,35 @@
 Contains <Cache> class with Redis database.
 """
 import redis
+from functools import wraps
 from uuid import uuid4
 from typing import Union, Optional, Callable
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator for <method>,
+    that keeps count of the calls to <method>.
+
+    Makes a calls dict to keep track of the <method>'s
+    <__qualname__> and the amount of times it was called,
+
+    makes <increment(*args, **kwargs)>, that's decorated
+    with <wraps(method)>,
+    that increments <method>'s counter
+    and returns <method(*args, **kwargs)>.
+
+    Returns the <increment> inner function,
+    which should have the closure for the calls counter.
+    """
+    calls = {method.__qualname__: 0}
+
+    @wraps(method)
+    def increment(*args, **kwargs):
+        calls[method.__qualname__] += 1
+        return method(*args, **kwargs)
+
+    return property(increment)
 
 
 class Cache:
@@ -12,6 +39,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores <data> as a value with
