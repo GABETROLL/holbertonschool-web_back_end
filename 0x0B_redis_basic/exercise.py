2a8619617bd8cc98390967651bf3aeb93b805a31
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-31 12:06:20 -0400

    Add format conversion function

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Contains <Cache> class with Redis database.
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -25,10 +25,42 @@ class Cache:
         self._redis.set(KEY, data)
         return KEY
 
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """
+        <get>s the value for <key> in <self>'s DB.
+
+        If <fn> is defined, this method uses <fn>
+        to convert the value to the user's desired format.
+        """
+        result = self._redis.get(key)
+
+        if fn is not None:
+            result = fn(result)
+
+        return result
+
+    def get_str(self, key):
+        """
+        Returns <self.get(key)> with the <fn>
+        being defined to return the string form
+        of the value (fn=lambda x: x.decode()).
+        """
+        return self.get(key, lambda x: x.decode())
+
+    def get_int(self, key):
+        """
+        Returns <self.get(key)> with the <fn>
+        being defined to return the int form
+        of the value (fn=lambda x: int(x.decode())).
+        """
+        return self.get(key, lambda x: int(x.decode()))
+
 
 if __name__ == "__main__":
     cache = Cache()
-    KEY = cache.store("Hello, World!")
+    KEY = cache.store(3)
     print(f"KEY: {KEY}")
-    VALUE = cache._redis.get(KEY)
-    print(f"VALUE: {VALUE}")
+    print(f"VALUE: {cache.get(KEY)}")
+    print(f"CONVERTED_VALUE: {cache.get(KEY, lambda x: x.decode().split(', '))}")
+    print(f"STR VALUE: {cache.get_str(KEY)}")
+    print(f"INT_VALUE: {cache.get_int(KEY)}")
