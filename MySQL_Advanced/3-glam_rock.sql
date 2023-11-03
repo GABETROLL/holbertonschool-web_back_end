d6dc7e1e30ad9157c666dd9513b79358f350d852
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-03 19:11:29 -0400

    Correct exercise 3 by using 2020 as the split year

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -4,8 +4,11 @@
 --  Use the 'metal_bands.sql' table dump
 --  Column names must be: band_name and lifespan (in years)
 --  You should use attributes formed and split for computing the lifespan
+--   (hint: you can just subtract the years)
+--   (IF THE BAND NEVER SPLIT, USE THE YEAR 2020)
 --  Your script can be executed on any database
-SELECT band_name
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
         FROM metal_bands
         WHERE style LIKE "%Glam rock%"
+        ORDER BY lifespan DESC
 ;
