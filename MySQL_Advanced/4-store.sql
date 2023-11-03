6cc4ede69de2c24b880fbf7e1e0aa7154eda39af
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-03 14:16:19 -0400

    Add comments for 4th exercise of MySQL Advanced and add its init file

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,3 +1,8 @@
+-- Create a trigger that decreases the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+-- (the tables for the items on stock and orders
+-- are in '4-init.sql',
+-- and are named 'items' and 'orders')
 CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders FOR EACH ROW
 BEGIN
