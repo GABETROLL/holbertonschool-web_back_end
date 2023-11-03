0b783bcc9ecabd0b0f135c7357d9a23de9de94e8
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-03 12:31:53 -0400

    Add ORDER BY SUM(fans) to 2-fans.sql

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -3,9 +3,10 @@
 -- Requirements:
 --  Column names must be: origin and nb_fans
 --  Your script can be executed on any database
-SELECT
-    origin    AS origin,
-    SUM(fans) AS nb_fans
-    FROM metal_bands
-    GROUP BY origin
+SELECT 
+origin    AS origin,
+SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY SUM(fans)
 ;
