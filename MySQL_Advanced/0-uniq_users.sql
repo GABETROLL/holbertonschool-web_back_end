7c05978d6a05fba00693b417e364940c64b9fe43
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-03 12:25:57 -0400

    Fix exercise 0 to meet the email column requirements

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -4,7 +4,7 @@
 -- name, string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     PRIMARY KEY (id)
 );
