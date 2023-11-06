edac2cd23eefa3042e09950d1c524313da179a0a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-06 14:15:35 -0400

    Add exercise 8 of MYSQL advanced

---
+++ MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,15 @@
+-- creates an index idx_name_first on the table names and the first letter of name.
+-- Requirements:
+    -- Only the first letter of name must be indexed
+
+-- (We were given a massive file with a table ``names``
+-- that looks like this:
+
+-- CREATE TABLE `names` (
+-- `name` varchar(255) DEFAULT NULL,
+-- `score` int(11) DEFAULT NULL
+--) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE INDEX idx_name_first
+    ON names (name(1))
+;
