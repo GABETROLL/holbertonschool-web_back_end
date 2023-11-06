34a1b8d623c0ed16a1b7322543afe073254bf5de
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-06 13:25:39 -0400

    Use FLOAT for MySQL exercise 10 instead

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -7,7 +7,7 @@
     -- And returns a / b or 0 if b == 0
 DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
-    RETURNS DECIMAL(20, 16) DETERMINISTIC
+    RETURNS FLOAT DETERMINISTIC
     BEGIN
         RETURN IF(b=0, 0, a/b);
     END
