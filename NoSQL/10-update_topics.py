98f0b8a8b225c93c51612a48e88cea0849cbd841
root <=>
2023-11-08 09:28:38 -0800

    Add missing shebang to NoSQL exercise 10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Write a Python function that changes all topics of a school document based on the name:
     - Prototype: def update_topics(mongo_collection, name, topics):
