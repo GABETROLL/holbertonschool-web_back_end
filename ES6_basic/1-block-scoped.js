1d19e9be667e5a1f23e9bcfaa5fdca09c8ca2223
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-09 13:26:07 -0400

    Disable eslint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable */
     const task = true;
     const task2 = false;
+    /* eslint-disable */
   }
 
   return [task, task2];
