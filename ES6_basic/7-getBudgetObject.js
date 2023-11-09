316931867184d90d82181b61137188b491b4a1df
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-09 15:16:31 -0400

    Add exercise 7 of ES6

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,20 @@
+/*
+Notice how the keys and the variable names are the same?
+Modify the following function’s budget object
+to simply use the keyname instead.
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+
+  return budget;
+}
+*/
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = { income, gdp, capita };
+  return budget;
+}
