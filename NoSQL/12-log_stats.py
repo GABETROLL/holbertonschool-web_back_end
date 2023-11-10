0ef33910264012c0ecde85e0d93e4ac31b56e47c
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-10 11:54:38 -0400

    Reset NoSQL exercise 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,45 +1,22 @@
 #!/usr/bin/env python3
 """
 Write a Python script that provides some stats
-about Nginx logs stored in MongoDB:
-    - Database: logs
-    - Collection: nginx
-    - Display (same as the example):
-        - first line: x logs where x is the number of documents
-            in this COLLECTION
-        - second line: Methods:
-        - 5 lines with the number of documents with
-            the method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-            in this order (see example below - warning:
-                it’s a tabulation before each line)
-        - one line with the number of documents with:
-            - method=GET
-            - path=/status
-
-You can use this dump as data sample: dump.zip
+about Nginx logs stored in MongoDB.
 """
 import pymongo
 
 
-def main():
-    """
-    Runs the task.
-    """
-    CLIENT = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-    DB = CLIENT.logs
-    COLLECTION = DB.nginx
+CLIENT = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+DB = CLIENT.logs
+COLLECTION = DB.nginx
 
-    LOG_COUNT = COLLECTION.count_documents({})
-    print(f"{LOG_COUNT} logs")
+LOG_COUNT = COLLECTION.count_documents({})
+print(f"{LOG_COUNT} logs")
 
-    print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        METHOD_COUNT = COLLECTION.count_documents({"method": method})
-        print(f"\tmethod {method}: {METHOD_COUNT}")
+print("Methods:")
+for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    METHOD_COUNT = COLLECTION.count_documents({"method": method})
+    print(f"\tmethod {method}: {METHOD_COUNT}")
 
-    STATUS_LOG_COUNT = COLLECTION.count_documents({"path": "/status"})
-    print(f"{STATUS_LOG_COUNT} status check")
-
-
-if __name__ == "__main__":
-    main()
+STATUS_LOG_COUNT = COLLECTION.count_documents({"path": "/status"})
+print(f"{STATUS_LOG_COUNT} status check")
