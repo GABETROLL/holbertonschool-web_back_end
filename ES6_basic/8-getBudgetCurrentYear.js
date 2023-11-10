3d71e373d52e5f2015d505fc266c6ef24f710aa5
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-10 11:03:06 -0400

    Remove coment from exercise 8 of ES6'

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,20 +1,6 @@
 /*
 Rewrite the `getBudgetForCurrentYear` function
 to use ES6 computed property names on the budget object.
-
-function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
-
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
-  return budget;
 }
 */
 
