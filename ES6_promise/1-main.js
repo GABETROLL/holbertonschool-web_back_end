b1441f0131b984e938c57c4481c609e2f79c2069
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-13 15:23:00 -0400

    Add ES6 promise exercise 1

---
+++ ES6_promise/1-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import getFullResponseFromAPI from './1-promise.js';
+/* eslint-enable */
+
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
