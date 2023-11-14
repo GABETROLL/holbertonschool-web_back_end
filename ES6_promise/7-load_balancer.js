1d05be122c54d62f681074d6886ae161d3e5b316
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-14 14:59:29 -0400

    Add ES6 promises exercise 7'

---
+++ ES6_promise/7-load_balancer.js
@@ -0,0 +1,37 @@
+/*
+Write and export a function named loadBalancer. It should accept two arguments chinaDownload (Promise) and USDownload (Promise).
+
+The function should return the value returned by the promise that resolved the first.
+
+export default function loadBalancer(chinaDownload, USDownload) {
+
+}
+
+bob@dylan:~$ cat 7-main.js
+import loadBalancer from "./7-load_balancer";
+
+const ukSuccess = 'Downloading from UK is faster';
+const frSuccess = 'Downloading from FR is faster';
+
+const promiseUK = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 100, ukSuccess);
+});
+
+const promiseUKSlow = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 400, ukSuccess);
+});
+
+const promiseFR = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 200, frSuccess);
+});
+
+const test = async () => {
+    console.log(await loadBalancer(promiseUK, promiseFR));
+    console.log(await loadBalancer(promiseUKSlow, promiseFR));
+}
+
+test();
+*/
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.any([chinaDownload, USDownload]);
+}
