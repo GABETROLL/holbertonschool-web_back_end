30495275b8d0abd56bc95106800f58643687be1a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-14 12:28:20 -0400

    Use different console.log strategy on ES6 promises exercise 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   function handleResolve(result) {
-    console.log(result[0].body, result[1].firstName, result[1].lastName);
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
   }
   function handleError(error) { // eslint-disable-line no-unused-vars
     console.log('Signup system offline');
