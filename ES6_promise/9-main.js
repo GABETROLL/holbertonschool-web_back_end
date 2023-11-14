7f0415ab83fe447a61ca72bc1349c9fb0423b89e
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-14 18:15:40 -0400

    Add ES6 exercise 9

---
+++ ES6_promise/9-main.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import guardrail from './9-try.js';
+import divideFunction from './8-try.js';
+/* eslint-enable */
+
+/*
+Expected output:
+
+[ 5, 'Guardrail was processed' ]
+[ 'Error: cannot divide by 0', 'Guardrail was processed' ]
+*/
+console.log(guardrail(() => { return divideFunction(10, 2)}));
+console.log(guardrail(() => { return divideFunction(10, 0)}));
---
+++ ES6_promise/9-try.js
@@ -0,0 +1,36 @@
+/*
+Write a function named guardrail
+that will accept one argument named ``mathFunction`` (Function).
+
+This function should create and return an array named queue.
+
+When the mathFunction function is executed,
+the value returned by the function should be appended to the queue.
+If this function throws an error,
+the error message should be appended to the queue.
+In every case, the message 'Guardrail was processed'
+should be added to the queue.
+
+Examples: (``divideFunction`` is from the previous exercise) 
+
+guardrail(() => { return divideFunction(10, 2)})
+outputs:
+[ 1000, 'Guardrail was processed' ]
+
+guardrail(() => { return divideFunction(10, 0)})
+outputs:
+[ 'Error: cannot divide by 0', 'Guardrail was processed' ]
+*/
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try{
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error.message);
+  }
+
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
