e723c27ad437401f8e18cac835aad00770241ff6
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-14 17:29:44 -0400

    Fix file extension to test file 7

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -3,12 +3,6 @@ Write and export a function named loadBalancer.
 It should accept two arguments chinaDownload (Promise) and USDownload (Promise).
 
 The function should return the value returned by the promise that resolved the first.
-
-Prototype:
-
-export default function loadBalancer(chinaDownload, USDownload) {
-
-}
 */
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.any([chinaDownload, USDownload]);
