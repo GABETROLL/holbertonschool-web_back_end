0321b91bddf7b33c8ca319362972852aa5c26e71
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-15 12:45:27 -0400

    Lint exercise 0

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -4,7 +4,7 @@ Implement a class named ClassRoom:
   - It should accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize
 */
 export default class ClassRoom {
-  constructor (maxStudentsSize) {
+  constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
