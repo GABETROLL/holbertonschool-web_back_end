c8c12287d979c3bc86908bbc0268e10ee137a52c
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-15 12:42:48 -0400

    Add ES6_classes exercise 1

---
+++ ES6_classes/1-main.js
@@ -0,0 +1,11 @@
+/*
+Expected output in stdout:
+[
+  ClassRoom { _maxStudentsSize: 19 },
+  ClassRoom { _maxStudentsSize: 20 },
+  ClassRoom { _maxStudentsSize: 34 }
+]
+*/
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
