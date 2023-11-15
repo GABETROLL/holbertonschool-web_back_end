090d8ece25fb237db9705996880b5f9c80ac0899
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-15 16:29:16 -0400

    Add ES6 classes exercise 6

---
+++ ES6_classes/6-main.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import SkyHighBuilding from './6-sky_high.js';
+/* eslint-enable */
+
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
