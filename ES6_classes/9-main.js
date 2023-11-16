0adc39b3be810420897c552f0b40e63c9a42742c
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-15 20:40:51 -0400

    Lint ES6 classes exercise 9 by removing the comment

--- ES6_classes/9-main.js
+++ ES6_classes/9-main.js
@@ -42,7 +42,7 @@ import listOfStudents from "./9-hoisting.js";
 console.log(listOfStudents);
 
 const listPrinted = listOfStudents.map(
-    student => student.fullStudentDescription
+  (student) => student.fullStudentDescription,
 );
 
 console.log(listPrinted);
