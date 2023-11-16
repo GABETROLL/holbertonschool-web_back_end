77f1925be03809718aaeb265f3179c217586005c
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-15 20:32:46 -0400

    Add ES6 classes exercise 9

---
+++ ES6_classes/9-main.js
@@ -0,0 +1,48 @@
+/*
+Expected output in stdout:
+[
+  StudentHolberton {
+    _firstName: 'Guillaume',
+    _lastName: 'Salva',
+    _holbertonClass: HolbertonClass { _year: 2020, _location: 'San Francisco' }
+  },
+  StudentHolberton {
+    _firstName: 'John',
+    _lastName: 'Doe',
+    _holbertonClass: HolbertonClass { _year: 2020, _location: 'San Francisco' }
+  },
+  StudentHolberton {
+    _firstName: 'Albert',
+    _lastName: 'Clinton',
+    _holbertonClass: HolbertonClass { _year: 2019, _location: 'San Francisco' }
+  },
+  StudentHolberton {
+    _firstName: 'Donald',
+    _lastName: 'Bush',
+    _holbertonClass: HolbertonClass { _year: 2019, _location: 'San Francisco' }
+  },
+  StudentHolberton {
+    _firstName: 'Jason',
+    _lastName: 'Sandler',
+    _holbertonClass: HolbertonClass { _year: 2019, _location: 'San Francisco' }
+  }
+]
+[
+  'Guillaume Salva - 2020 - San Francisco',
+  'John Doe - 2020 - San Francisco',
+  'Albert Clinton - 2019 - San Francisco',
+  'Donald Bush - 2019 - San Francisco',
+  'Jason Sandler - 2019 - San Francisco'
+]
+*/
+/* eslint-disable */
+import listOfStudents from "./9-hoisting.js";
+/* eslint-enable */
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
+
+console.log(listPrinted);
