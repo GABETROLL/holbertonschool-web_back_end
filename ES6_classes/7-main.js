acfaa6bf251218dd8119b3156ee2bdd424f59401
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-17 14:28:18 -0400

    Add ES6 classes exercise 7, even if it's wrong

---
+++ ES6_classes/7-main.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import Airport from './7-airport.js';
+/* eslint-enable */
+
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
