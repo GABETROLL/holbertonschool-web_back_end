0a01e57977ac6e3d996b9ef21de4377d0410d2ca
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-20 15:20:02 -0400

    Add exercise 3 of ES6 Arrays

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,17 @@
+/*
+Create a function, named ``getStudentIdsSum``,
+  that returns the sum of all the student ids.
+It should accept
+  a list of students (from ``getListStudents``) as a parameter.
+
+You must use the ``reduce`` function on the array. 
+*/
+export default function getStudentIdsSum(students) {
+  if (!(students instanceof Array)) {
+    return [];
+  }
+  return students.reduce(
+    ((result, student) => result + student.id),
+    0,
+  );
+}
