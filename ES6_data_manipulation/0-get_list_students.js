04ac4503e5cd51627058f91d0b748fd43c2db80d
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-20 14:40:40 -0400

    Add ES6 arrays exercises

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,31 @@
+/*
+Create a function named getListStudents
+  that returns an array of objects.
+Each object should have three attributes:
+  id (Number),
+  firstName (String),
+  and location (String).
+The array contains the following students in order:
+  - Guillaume, id: 1, in San Francisco
+  - James, id: 2, in Columbia
+  - Serena, id: 5, in San Francisco
+*/
+export default function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+}
