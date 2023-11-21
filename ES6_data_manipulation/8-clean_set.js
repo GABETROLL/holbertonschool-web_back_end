68b4d075105caf50cd7dafe0af5c6c0c56c0b83f
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-21 14:02:45 -0400

    Check item type in exercise 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -20,7 +20,7 @@ export default function cleanSet(set, startString) {
   const resultArray = [];
 
   set.forEach((item) => {
-    if (item.startsWith(startString)) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
       resultArray.push(
         item.substring(
           startString.length, item.length,
--- ES6_data_manipulation/8-main.js
+++ ES6_data_manipulation/8-main.js
@@ -13,3 +13,4 @@ console.log('string'.startsWith(''));
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+console.log(cleanSet(new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']), 'id-'));
