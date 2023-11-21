193247e25a7c6f75e6fd00eb4a3710eeb26e26cd
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-21 12:17:12 -0400

    Correct ES6 Arrays exercise 5 by using ArrayBuffer and DataView

--- ES6_data_manipulation/5-main.js
+++ ES6_data_manipulation/5-main.js
@@ -2,6 +2,5 @@
 import createInt8TypedArray from "./5-typed_arrays.js";
 
 console.log(createInt8TypedArray(10, 2, 89));
-// console.log(createInt8TypedArray(10, 11, 89));
-// (ERRORS)
 console.log(createInt8TypedArray(10, 2, 1000000));
+console.log(createInt8TypedArray(10, 11, 89));
--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,22 @@
+/*
+Create a function, named ``createInt8TypedArray``,
+  that returns a new ``ArrayBuffer``
+  with an Int8 value at a specific position.
+
+It should accept three arguments:
+  ``length`` (Number),
+  ``position`` (Number),
+  and ``value`` (Number).
+
+If adding the value is not possible,
+  the error ``Position outside range`` should be thrown.
+*/
 export default function createInt8TypedArray(length, position, value) {
   const result = new DataView(new ArrayBuffer(length));
-  result.setInt8(position, value);
+  try {
+    result.setInt8(position, value);
+  } catch (error) {
+    throw new Error('Position outside range');
+  }
   return result;
 }
