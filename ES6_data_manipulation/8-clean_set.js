d2ee0d6b0779fcb83fce0c6b759663c6044c5aac
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-21 13:45:17 -0400

    Add ES6 Arrays exercise 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,29 @@
+/*
+Create a function, named ``cleanSet``,
+  that returns a string of all the set values
+  that start with a specific string (``startString``).
+
+It accepts two arguments:
+  a ``set`` (Set)
+  and a ``startString`` (String).
+
+When a value starts with ``startString``,
+  you only append the rest of the string.
+  The string contains all the values of the set,
+  separated by '-'. 
+*/
+export default function cleanSet(set, startString) {
+  let resultArray = [];
+
+  set.forEach((item) => {
+    if (item.startsWith(startString)) {
+      resultArray.push(
+        item.substring(
+          startString.length, item.length
+        )
+      );
+    }
+  });
+
+  return resultArray.join('-');
+}
---
+++ ES6_data_manipulation/8-main.js
@@ -0,0 +1,10 @@
+import cleanSet from "./8-clean_set.js";
+
+console.log((new Set([0, 1, 2])).entries());
+// (new Set(...)).entries() doesn't return an array...
+// And Array(new Set(...)) doesn't give you an array of
+//  all of the set's items...
+// Neither does Array((new Set(...)).entries()) .
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
