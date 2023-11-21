aa6c3efad210ba9ae683d32af89043ebdfecb311
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-21 12:51:38 -0400

    Make ES6 Arrays exercise 6's more informative

--- ES6_data_manipulation/6-main.js
+++ ES6_data_manipulation/6-main.js
@@ -1,4 +1,14 @@
 /* eslint-disable import/extensions */
 import setFromArray from "./6-set.js";
 
-console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+const result = setFromArray([
+  12, 0.2, -3, -3.14, NaN, -NaN, Infinity, -Infinity,
+  'hi', '',
+  true, false, null, undefined, -null, -undefined, -false, -true,
+  [], [-1], [undefined],
+  {}, {hi: 'hello', method() { return NaN; }},
+  Symbol(), Symbol('hi'),
+  new Int8Array(10), new ArrayBuffer(10), new DataView(new ArrayBuffer(1)),
+]);
+
+console.log(result);
