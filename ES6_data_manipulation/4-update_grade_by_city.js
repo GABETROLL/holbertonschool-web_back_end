b0c1489c3e057a256c2c36876b40ee8c9524eb1b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-21 11:22:41 -0400

    Add exercise 4 of ES6 Arrays

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,36 @@
+/*
+Create a function ``updateStudentGradeByCity``
+  that returns an array of students for a specific city
+  with their new grade.
+
+It should accept a list of students (from ``getListStudents``),
+  a ``city`` (String),
+  and ``newGrades`` (Array of “grade” objects)
+  as parameters.
+
+``newGrades`` is an array of objects with this format:
+  {
+    studentId: 31,
+    grade: 78,
+  }
+
+If a student doesn’t have grade in ``newGrades``,
+  the final grade should be 'N/A'.
+
+You must use ``filter`` and ``map`` combined.
+*/
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newStudent = newGrades
+        .find((newStudent) => newStudent.studentId === student.id);
+
+      return {
+        ...student,
+        grade: newStudent && newStudent.grade
+          ? newStudent.grade : 'N/A',
+      };
+    });
+}
