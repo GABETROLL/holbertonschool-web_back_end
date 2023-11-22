11d68e33d2371c0740ffeaf373c09425c24ce246
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-22 18:31:35 -0400

    Add NodeJS exercise 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200);
+  response.end('Hello Holberton School!');
+});
+app.listen(1245, 'localhost');
