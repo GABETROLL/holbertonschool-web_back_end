6b925ba28d54d3b9be7ccdbd8509ae29d604ddfe
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-23 19:30:38 -0400

    Respond with the correct error message

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const studentsTextOutput = require('./2-3-5-7-8-get_students_text');
 
 const app = http.createServer((request, response) => {
+  const preamble = 'This is the list of our students\n';
+
   if (process.argv.length <= 1) {
     response.writeHead(500);
-    response.end('Cannot load the database');
+    response.end(`${preamble}Cannot load the database`);
   } else if (request.url === '/') {
     response.writeHead(200);
     response.end('Hello Holberton School!');
-  } else if (request.url === '/students') {
+  } else if (request.url === '/students' || request.url === '/students/') {
     const databaseFileName = process.argv[2];
 
     let data;
@@ -17,13 +19,13 @@ const app = http.createServer((request, response) => {
       data = fs.readFileSync(databaseFileName, 'utf8');
     } catch (error) {
       response.writeHead(500);
-      response.end('Cannot load the database');
+      response.end(`${preamble}Cannot load the database`);
       return;
     }
 
     response.writeHead(200);
     const textOutput = studentsTextOutput(data).join('\n');
-    response.end(`This is the list of our students\n${textOutput}`);
+    response.end(`${textOutput}`);
   } else {
     response.writeHead(404);
     response.end('Invalid URL');
