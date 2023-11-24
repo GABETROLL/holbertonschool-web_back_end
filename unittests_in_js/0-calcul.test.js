6bad4648b260ba985cee7bf3713c09c116e1cb03
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-24 19:41:31 -0400

    Fix JS tests exercise 0 to have multiple test suites for each amount of numbers rounded

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,39 +2,48 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('sums rounded ``a`` and rounded ``b``, as ``Math.floor`` rounds', () => {
-    // (1.X) + (1.X)
+  it('returns ``a + b`` when both ``a`` and ``b`` are whole nnumbers', () => {
     assert.equal(calculateNumber(1, 1), 2);
-    assert.equal(calculateNumber(1.25, 1), 2);
-    assert.equal(calculateNumber(1, 1.25), 2);
-    assert.equal(calculateNumber(1.25, 1.25), 2);
-    assert.equal(calculateNumber(1.5, 1), 3);
-    assert.equal(calculateNumber(1, 1.5), 3);
-    assert.equal(calculateNumber(1.5, 1.5), 4);
-    // (-1.X) + (-1.X)
     assert.equal(calculateNumber(-1, -1), -2);
-    assert.equal(calculateNumber(-1.25, -1), -2);
-    assert.equal(calculateNumber(-1, -1.25), -2);
-    assert.equal(calculateNumber(-1.25, -1.25), -2);
-    assert.equal(calculateNumber(-1.5, -1), -2);
-    assert.equal(calculateNumber(-1, -1.5), -2);
-    assert.equal(calculateNumber(-1.5, -1.5), -2);
-    // (2.X) + (2.X)
     assert.equal(calculateNumber(2, 2), 4);
-    assert.equal(calculateNumber(2.25, 2), 4);
-    assert.equal(calculateNumber(2, 2.25), 4);
-    assert.equal(calculateNumber(2.25, 2.25), 4);
-    assert.equal(calculateNumber(2.5, 2), 5);
-    assert.equal(calculateNumber(2, 2.5), 5);
-    assert.equal(calculateNumber(2.5, 2.5), 6);
-    // (-2.X) + (-2.X)
     assert.equal(calculateNumber(-2, -2), -4);
+  });
+  it('rounds ``a``, like ``Math.round``, before adding it', () => {
+    assert.equal(calculateNumber(1.25, 1), 2);
+    assert.equal(calculateNumber(1.5, 1), 3);
+
+    assert.equal(calculateNumber(-1.25, -1), -2);
+    assert.equal(calculateNumber(-1.5, -1), -2);
+  
+    assert.equal(calculateNumber(2.25, 2), 4);
+    assert.equal(calculateNumber(2.5, 2), 5);
+
     assert.equal(calculateNumber(-2.25, -2), -4);
-    assert.equal(calculateNumber(-2, -2.25), -4);
-    assert.equal(calculateNumber(-2.25, -2.25), -4);
     assert.equal(calculateNumber(-2.5, -2), -4);
+  });
+  it('rounds ``b``, like ``Math.round``, before adding it', () => {
+    assert.equal(calculateNumber(1, 1.25), 2);
+    assert.equal(calculateNumber(1, 1.5), 3);
+    assert.equal(calculateNumber(-1, -1.25), -2);
+    assert.equal(calculateNumber(-1, -1.5), -2);
+
+    assert.equal(calculateNumber(2, 2.25), 4);
+    assert.equal(calculateNumber(2, 2.5), 5);
+    assert.equal(calculateNumber(-2, -2.25), -4);
     assert.equal(calculateNumber(-2, -2.5), -4);
+  });
+  it('rounds ``a`` and ``b``, like ``Math.round``, before adding them to eachother', () => {
+    assert.equal(calculateNumber(1.25, 1.25), 2);
+    assert.equal(calculateNumber(1.5, 1.5), 4);
+    assert.equal(calculateNumber(-1.25, -1.25), -2);
+    assert.equal(calculateNumber(-1.5, -1.5), -2);
+
+    assert.equal(calculateNumber(2.25, 2.25), 4);
+    assert.equal(calculateNumber(2.5, 2.5), 6);
+    assert.equal(calculateNumber(-2.25, -2.25), -4);
     assert.equal(calculateNumber(-2.5, -2.5), -4);
+  });
+  it('returns Infinity and NaN when adding Infinities and NaNs', () => {
     // Infinities
     assert.equal(calculateNumber(Infinity, Infinity), Infinity);
     assert.equal(calculateNumber(-Infinity, Infinity), NaN);
