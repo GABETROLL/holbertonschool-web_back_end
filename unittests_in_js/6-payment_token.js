f2037c293fa299274c66e6923f58a0b32a7e2434
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-25 00:04:45 -0400

    Add correct text output for JS Tests exercise 6

--- unittests_in_js/6-payment_token.js
+++ unittests_in_js/6-payment_token.js
@@ -1,7 +1,7 @@
 module.exports = function getPaymentTokenFromAPI(success) {
   if (success) {
     return Promise.resolve({
-      data: ' response from the API',
+      data: 'Successful response from the API',
     });
   }
 };
