fea59ef38cea8d0026954e152e8c0194665f93f8
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-24 22:02:23 -0400

    Corect JS Tests exercise 3 by using proper sandbox setups and eql instead of equal

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -4,16 +4,29 @@ const sendPaymentRequestToApi = require('./3-payment');
 const chai = require('chai');
 
 describe('sendPaymentRequestToApi', () => {
-  it('uses ``Utils.calculateNumber`` to calculate the total', () => {
-    const sandbox = sinon.createSandbox();
-    sinon.spy(Utils.calculateNumber);
+  const sandbox = sinon.createSandbox();
 
+  beforeEach(() => {
+    sandbox.spy(Utils);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('uses ``Utils.calculateNumber`` to calculate the total', () => {
     const args = [2, 4];
     sendPaymentRequestToApi(...args);
 
-    chai.expect(Utils.calculateNumber.calledOnce()).to.be.true;
-    chai.expect(Utils.calculateNumber.getCall(0).args).to.equal(args);
-
-    sandbox.restore();
+    chai.expect(Utils.calculateNumber.calledOnce).to.be.true;
+    // DO NOT USE '.to.equal', THAT WILL FAIL.
+    // USE '.to.eql', WHICH CHECKS IF THE ARRAY'S
+    // CONTENTS ARE THE SAME, INSTEAD OF JS'
+    // [...] === [...] or [...] == [...],
+    // WHICH CHECKS IF THE ARRAYS ARE THE SAME
+    // OBJECT, SINCE ARRAYS ARE OBJECTS,
+    // AND OBJECTS ARE ONLY EQUAL IF THEY HAVE THE
+    // SAME IDENTITY (ADDR).
+    chai.expect(Utils.calculateNumber.getCall(0).args).to.eql(['SUM', ...args]);
   });
 });
