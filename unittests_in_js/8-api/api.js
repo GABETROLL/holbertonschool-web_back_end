892d02970f31665cf124ec5bb0dd77c400b90906
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-25 15:05:07 -0400

    Add exercise 8 of JS Tests

---
+++ unittests_in_js/8-api/api.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+app.listen(7865, () => console.log('API available on localhost port 7865'));
---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const chai = require('chai');
+
+describe('index page', () => {
+  it('responds with ``Welcome to the payment system``, when requested with ``GET /``', (done) => {
+    request('http://localhost:7865/', (error, response, body) => {
+      if (error) {
+        done(error);
+      } else {
+        try{
+          chai.expect(body).to.equal('Welcome to the payment system');
+        } catch (error) {
+          done(error);
+        }
+        done();
+      }
+    });
+  });
+});
