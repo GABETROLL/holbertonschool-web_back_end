141408c6bbb90b3274278e1f6830288945d1cc36
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-27 11:23:45 -0400

    Add studentsTextOutput to exercise 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,5 +1,60 @@
 const fs = require('fs');
-const studentsTextOutput = require('./2-3-5-7-8-get_students_text');
+
+function studentsTextOutput(data) {
+  // DATA SETUP:
+  const splitData = data
+    .split('\n')
+    .map((row) => row.split(','));
+
+  // The index of the header row in the CSV file,
+  // assuming that the first non-empty row is the header,
+  // and that the header has the field named: 'field'.
+  const headerRowIndex = splitData.findIndex((row) => row.length);
+  // The header row should have commas separating its fields,
+  // since it's a CSV file.
+  const columnNames = splitData[headerRowIndex];
+  // The students are all of the following rows that are not empty.
+  // They are stored an Array of student objects,
+  // with the column names as keys,
+  // and the student's row's values as values.
+  const students = splitData
+    .slice(headerRowIndex + 1)
+    .filter((row) => row.length === columnNames.length)
+    .map((student) => {
+      const result = {};
+
+      for (const columnIndex in columnNames) {
+        if (Object.hasOwn(columnNames, columnIndex)) {
+          const column = columnNames[columnIndex];
+          const studentColumnValue = student[columnIndex];
+          result[column] = studentColumnValue;
+        }
+      }
+
+      return result;
+    });
+
+  // A map of all of the fields that the students study,
+  // and the list of the students that study it.
+  const fields = new Map();
+  for (const student of students) {
+    if (fields.has(student.field)) {
+      fields.get(student.field).push(student.firstname);
+    } else {
+      fields.set(student.field, [student.firstname]);
+    }
+  }
+
+  // TEXT OUTPUT SETUP:
+  const result = [`Number of students: ${students.length}`];
+  fields.forEach(
+    (fieldStudents, field) => result.push(
+      `Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`,
+    ),
+  );
+
+  return result;
+}
 
 module.exports = function countStudents(path) {
   let data;
@@ -9,6 +64,7 @@ module.exports = function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 
-  studentsTextOutput(data)
-    .forEach((line) => console.log(line));
+  for (const line of studentsTextOutput(data)) {
+    console.log(line);
+  }
 };
