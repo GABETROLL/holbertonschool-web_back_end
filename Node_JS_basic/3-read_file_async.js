47ccabac5bd049881ed80894cdef68b2db167cad
GABETROLL <gabriel7icplhappy@gmail.com>
2023-11-27 11:45:35 -0400

    Add function to file 3 and add old database file

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,61 @@
 /* eslint-disable import/no-unresolved */
 const fsPromsises = require('fs/promises');
-const studentsTextOutput = require('./2-3-5-7-8-get_students_text');
+
+function studentsTextOutput(data) {
+  // DATA SETUP:
+  const splitData = data
+    .split('\n')
+    .map((row) => row.split(','));
+
+  // The index of the header row in the CSV file,
+  // assuming that the first non-empty row is the header,
+  // and that the header has the field named: 'field'.
+  const headerRowIndex = splitData.findIndex((row) => row.length);
+  // The header row should have commas separating its fields,
+  // since it's a CSV file.
+  const columnNames = splitData[headerRowIndex];
+  // The students are all of the following rows that are not empty.
+  // They are stored an Array of student objects,
+  // with the column names as keys,
+  // and the student's row's values as values.
+  const students = splitData
+    .slice(headerRowIndex + 1)
+    .filter((row) => row.length === columnNames.length)
+    .map((student) => {
+      const result = {};
+
+      for (const columnIndex in columnNames) {
+        if (Object.hasOwn(columnNames, columnIndex)) {
+          const column = columnNames[columnIndex];
+          const studentColumnValue = student[columnIndex];
+          result[column] = studentColumnValue;
+        }
+      }
+
+      return result;
+    });
+
+  // A map of all of the fields that the students study,
+  // and the list of the students that study it.
+  const fields = new Map();
+  for (const student of students) {
+    if (fields.has(student.field)) {
+      fields.get(student.field).push(student.firstname);
+    } else {
+      fields.set(student.field, [student.firstname]);
+    }
+  }
+
+  // TEXT OUTPUT SETUP:
+  const result = [`Number of students: ${students.length}`];
+  fields.forEach(
+    (fieldStudents, field) => result.push(
+      `Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`,
+    ),
+  );
+
+  return result;
+}
 
 module.exports = function countStudents(path) {
   return fsPromsises.readFile(path, { encoding: 'utf8' })
@@ -8,5 +63,5 @@ module.exports = function countStudents(path) {
       studentsTextOutput(data)
         .forEach((line) => console.log(line));
     })
-    .catch(() => { throw new Error('Error: Cannot load the database'); });
+    .catch(() => { throw new Error('Cannot load the database'); });
 };
